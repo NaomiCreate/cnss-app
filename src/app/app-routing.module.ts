712ea6376bb1e1ff9c69411e08919e0da4318b0d
NaomiCreate <naomiresnik@gmail.com>
2020-12-13 20:30:00 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ContactListComponent } from './contact-list/contact-list.component';
+import { CreateTimerComponent } from './create-timer/create-timer.component';
+import { HistoryComponent } from './history/history.component';
+import { LoginComponent } from './login/login.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/login', pathMatch: 'full' },//empty string because we will see this page at the path: http://localhost:4200/
+  {path: 'login', component: LoginComponent },
+  {path: 'contact-list', component: ContactListComponent },
+  {path: 'create-timer', component: CreateTimerComponent },
+  {path: 'history', component: HistoryComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
