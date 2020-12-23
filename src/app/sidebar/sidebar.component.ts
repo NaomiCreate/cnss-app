178caddea14dadbfd553b4882c9de6426e786a86
NaomiCreate <naomiresnik@gmail.com>
2020-12-23 18:10:24 +0200

    Only a registered user will see the sidebar

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AuthService } from '../services/auth.service';//So the sidebar appears after the user registered
+import {Router} from '@angular/router';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
-  constructor() { }
+  constructor(public authservice: AuthService, private router: Router) { }
 
   ngOnInit(): void {
   }
