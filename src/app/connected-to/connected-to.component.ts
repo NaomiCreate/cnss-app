bf7d628818f267d733c09bd5929ffee890db4fab
mavishak <mayanavital@gmail.com>
2021-05-30 23:35:48 +0300

    connected-to

--- src/app/connected-to/connected-to.component.ts
+++ src/app/connected-to/connected-to.component.ts
@@ -6,7 +6,14 @@ import { Subscription } from 'rxjs';
 enum Status {
   StandBy,
   Accept,
-  // Deny
+  Deny
+}
+
+interface Connection{
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone:number; 
 }
 
 @Component({
@@ -16,69 +23,93 @@ enum Status {
 })
 export class ConnectedToComponent implements OnInit {
 
-  Status = Status;
-  state:Status = Status.StandBy
+  public state = Status; 
+  public connections_state:Status = Status.StandBy; // connections state
 
-  connections = [];
-  requests = [];
+  requests:Array<Connection>;
+  connections:Array<Connection>;
 
   message = '';
   errorMessage = ''; //validation error handle
   error: {name:string, message:string} = {name:'' , message:''}; //firebase error handle
   
-  subscription: Subscription;
+  subscriptions:Array<Subscription> = [];
 
   constructor(private authservice: AuthService,public crudservice:CrudService) { }
 
   ngOnInit() {
-    if(this.authservice.currentUser != null)//We will make sure the user is logged in
-    {
-      // Show connections (requests that confirmed)
-      this.subscription = this.crudservice.get_AllConnections(this.authservice.currentUserId).snapshotChanges().subscribe(data => {
-        this.connections = data.map(c => {
-          let connection={};
+
+    // Show connections (requests that confirmed)
+    this.subscriptions.push(this.crudservice.get_AllConnections(this.authservice.currentUserId).snapshotChanges()
+    .subscribe(data => {
+
+      this.connections_state=Status.StandBy;
+
+      this.connections = data.map(c => {
+
+          let connection:Connection = {
+            firstName: '',
+            lastName: '',
+            email: '',
+            phone: 0,
+          };
+
           this.crudservice.get_contact_details(c.payload.doc.id, c.payload.doc.data()['id'])
           .then((doc) => { 
-            connection['phone'] = doc.data()['phone'];
-            connection['firstName'] = doc.data()['firstName'];
-            connection['lastName'] = doc.data()['lastName'];
-            //connection['shareHistory'] = doc.data()['shareHistory'];
-            connection['email'] = doc.data()['email'];
-            //this.state=Status.Accept;
+            connection.phone = doc.data()['phone'];
+            connection.firstName = doc.data()['firstName'];
+            connection.lastName = doc.data()['lastName'];
+            connection.email = doc.data()['email'];
+            
           }).catch(error => {console.log(error)});
           return connection;
         })
-        this.state=Status.Accept;
-      });  
-      // Show requests (requests that didn't confirmed and didn't rejected)
-      this.subscription = this.crudservice.get_AllRequests(this.authservice.currentUserId).snapshotChanges().subscribe(data => {
-        this.requests = data.map(c => {
-          let request={};
 
-          this.crudservice.get_contact_details(c.payload.doc.id, c.payload.doc.data()['id'])
-          .then((doc) => { 
-           
-            request['phone'] = doc.data()['phone'];
-            request['firstName'] = doc.data()['firstName'];
-            request['lastName'] = doc.data()['lastName'];
-            //connection['shareHistory'] = doc.data()['shareHistory'];
-            request['email'] = doc.data()['email'];
+        // Show requests (requests that didn't confirmed and didn't rejected)
+        this.subscriptions.push(this.crudservice.get_AllRequests(this.authservice.currentUserId).snapshotChanges()
+        .subscribe(data => {
 
-            // this.state=Status.Accept;
-          
-          }).catch(error => {console.log(error)});
-          return request;
-        })
-        this.state=Status.Accept;
+            this.connections_state=Status.StandBy;
 
-      }); 
-    }
+            this.requests = data.map(c => {
+
+              let request:Connection = {
+                firstName: '',
+                lastName: '',
+                email: '',
+                phone: 0,
+              };
+
+              this.crudservice.get_contact_details(c.payload.doc.id, c.payload.doc.data()['id'])
+              .then((doc) => { 
+                
+                request['phone'] = doc.data()['phone'];
+                request['firstName'] = doc.data()['firstName'];
+                request['lastName'] = doc.data()['lastName'];
+                request['email'] = doc.data()['email'];
+              
+              }).catch(error => {console.log(error)});
+              return request;
+            })
+            
+            if(this.connections.length + this.requests.length > 0){
+              this.connections_state=Status.Accept;
+            }else{
+              this.connections_state=Status.Deny;
+            }
+            
+          })
+        )
+      }) 
+    );
+
+    
+    
   }
 
   //will fire after the user press "Delete Contact"
   DeleteConnection(email:string){
     if(confirm("Are you sure you want to delete this connection?")){
-      this.state=Status.StandBy;
       if(this.authservice.currentUser != null)//We will make sure the user is logged in
       {
         this.crudservice.get_uidFromEmail(email)
@@ -87,7 +118,6 @@ export class ConnectedToComponent implements OnInit {
                 this.crudservice.delete_contact(doc.data()[email], this.authservice.currentUserName)
                 this.crudservice.delete_connection(this.authservice.currentUserId, email);
               }
-              this.state=Status.Accept;
           }).catch(error => {console.log(error)});
       }
     }
@@ -97,7 +127,6 @@ export class ConnectedToComponent implements OnInit {
     //Delete Request from Users->[Y's uid]->requestes
     //Delete Contact from Users->[X's uid]->contacts
     if(confirm("Are you sure you want to reject this request?")){
-      this.state=Status.StandBy;
       //Delete Request
       this.crudservice.delete_request(this.authservice.currentUserId, emailRequestToReject);
       //Delete Contact
@@ -106,14 +135,12 @@ export class ConnectedToComponent implements OnInit {
           if(doc.exists){
             this.crudservice.delete_contact(doc.data()[emailRequestToReject], this.authservice.currentUserName);
           }
-          this.state=Status.Accept;
       }).catch(error => {console.log(error)});
     }
   }
   
   confirmRequest(emailToConfirm:string){
     if(confirm("Are you sure you want to confirm this request?")){
-      this.state=Status.StandBy;
       this.crudservice.get_uidFromEmail(emailToConfirm)
         .then((doc) => {
           if(doc.exists){
@@ -124,14 +151,14 @@ export class ConnectedToComponent implements OnInit {
             //udate contacts to be: confirmed: true
             this.crudservice.update_request_to_confirmed(doc.data()[emailToConfirm],this.authservice.currentUserName);
           }
-          this.state=Status.Accept;
       }).catch(error => {console.log(error)});
     }
   }
 
   ngOnDestroy(){
-    if(this.subscription != undefined)
-        this.subscription.unsubscribe();
+    this.subscriptions.forEach(subscription => {
+      subscription.unsubscribe();
+    });
   }
 
 }
