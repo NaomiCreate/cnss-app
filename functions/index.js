cc279a0588ef809d1de6c859b9b2631dd7aeb552
NaomiCreate <naomiresnik@gmail.com>
2021-06-22 13:13:04 +0300

    continuing emails

--- functions/index.js
+++ functions/index.js
@@ -20,81 +20,109 @@ const db = admin.firestore();
 
 const { SENDER_EMAIL, SENDER_PASSWORD } = process.env;
 
+const mailTransport = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: SENDER_EMAIL,
+    pass: SENDER_PASSWORD,
+  },
+});
+
 exports.test = functions.database.ref("/devices/{device_ID}/history/{alert_ID}")
-  .onWrite((snapshot, context) => {
+  .onWrite(
+    (snapshot, context) => { 
+
+      console.log('DEBUG:: IN ONWRITE');
+      sendMail(snapshot, context);
+
+      return true;
+    //   const { before, after } = snapshot;
+
+    //   if (before.val() == null) {//New alert created
+    //     functions.logger.info('context.params.device_ID =', context.params.device_ID);
+
+    //     // get owners uID from device ID
+    //     const deviceRef = db.collection('deviceToUid').doc(context.params.device_ID);
+    //     const uidDoc = await deviceRef.get();
+
+    //     if(!uidDoc.exists){
+    //       functions.logger.info("No such document!");
+    //       return;
+    //     }
+
+    //     console.log('owners uID:', uidDoc.data()[context.params.device_ID]);
+
+    //     // get users email from uID
+    //     const userRef = db.collection('users').doc(uidDoc.data()[context.params.device_ID]).collection('user-info');
+    //     const userInfo = await userRef.get();
+
+    //     if(!userInfo.exists){
+    //       functions.logger.info("No such collection!");
+    //       return;
+    //     }
+
+    //     const email = userInfo[0].id; // owners email
+
+
+    //     const mailOptions = {
+    //       from: 'CNSS <noreply@firebase.com',
+    //       to: email,
+    //       subject: 'Sending Email using Node.js',
+    //       text: 'That was easy!'
+    //     };
+
+    //     const info = await mailTransport.sendMail(mailOptions);
+    //     console.log("Message sent: ", info.messageId)
+      // }
+    }
+  );
+
+  async function sendMail(snapshot, context){
+
     const { before, after } = snapshot;
 
-    if (before.val() == null) {//New alert created
-      functions.logger.info('context.params.device_ID=', context.params.device_ID);
-      //Get the user's ID from the device ID:
-      const uidReff = db.collection('deviceToUid').doc(context.params.device_ID).get().then((uidRef) => {
-        if (!uidRef.exists) {
-          console.log('No such document!');
-          return null;
-        } else {
-          console.log('Document data[deviceID]:', uidRef.data()[context.params.device_ID]);
-          //Get the user's email from the user's ID:
-          const emailRef = db.collection('users').doc(uidRef.data()[context.params.device_ID]).collection('user-info').get().then((doc) => {
-            doc.forEach((emailAddress) => {
-              console.log('email=', emailAddress.id);
-              //Send alert email to the user's email:
-              const mailTransport = nodemailer.createTransport({
-                service: 'gmail',
-                auth: {
-                  user: SENDER_EMAIL,
-                  pass: SENDER_PASSWORD,
-                },
-              });
-              var mailOptions = {
-                from: 'CNSS <noreply@firebase.com',
-                to: emailAddress.id,
-                subject: 'Sending Email using Node.js',
-                text: 'That was easy!'
-              };
-              mailTransport.sendMail(mailOptions, function (error, info) {
-                if (error) {
-                  console.log(error);
-                } else {
-                  console.log('Email sent: ' + info.response);
-                }
-              });
-              return emailAddress.id;
-            });
-          });
-        }
-      });
-      functions.logger.info('created event');
-      return;
-    }
-    if (after.val() == null) {
-      functions.logger.info('deleted event');
-      return;
-    }
-    else {
-      functions.logger.info('update event');
-      return;
-    }
-  });
+    // new alert created
+    if (before.val() == null) {
 
-function getUidFromDeviceID(deviceID) {
-  const uidRef = db.collection('deviceToUid').doc(deviceID).get().then(() => {
-    if (!uidRef.exists) {
-      console.log('getUidFromDeviceID No such document!');
-      return null;
-    } else {
-      console.log('getUidFromDeviceID Document data[deviceID]:', uidRef.data()[deviceID]);
+      console.log('DEBUG:: NEW ALERT');
 
-      return uidRef.data()[deviceID];//the Uid
+      // get owners uID from device ID
+      const deviceRef = db.collection('deviceToUid').doc(context.params.device_ID);
+      const uidDoc = await deviceRef.get();
+
+      if(!uidDoc.exists){
+        functions.logger.info("No such document!");
+        return;
+      }
+
+
+      // get users email from uID
+      const userRef = db.collection('users').doc(uidDoc.data()[context.params.device_ID]).collection('user-info');
+      const userInfo = await userRef.get();
+
+      if(userInfo.empty){
+        functions.logger.info("No such collection!");
+        return;
+      }
+
+      const email = userInfo.docs[0].id; // owners email
+    
+      console.log('DEBUG:: FROM: ',SENDER_EMAIL);
+      console.log('DEBUG:: TO: ', email);
+
+      const mailOptions = {
+        from: 'CNSS <noreply@firebase.com',
+        to: email,
+        subject: 'Sending Email using Node.js',
+        text: 'That was easy!'
+      };
+
+      const info = await mailTransport.sendMail(mailOptions);
+      console.log("Message sent: ", info.messageId)
     }
-  });
-}
+  }
 
-function getEmailFromUid(uid) {
-  const emailRef = db.collection('users').doc(uid).collection('user-info').get().forEach((doc) => {
-    //console.log(doc.data()); // For data inside doc
-    console.log('doc.id=', doc.id); // For doc name
-    return doc.id;
-  });
-  return null;
-}
+  
+
+
 
